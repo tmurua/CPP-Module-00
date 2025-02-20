9b608e9b737093c516e48e1557e6547e5d379293
tmurua <tommurua@gmail.com>
2025-02-20 20:14:59 +0100

    feat: create get_input helper function to keep prompting in case of empty input. Also fixed initialization of current_contact with the use of a constructor.

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: tmurua <tmurua@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 15:59:12 by tmurua            #+#    #+#             */
-/*   Updated: 2025/02/20 16:51:18 by tmurua           ###   ########.fr       */
+/*   Updated: 2025/02/20 19:43:06 by tmurua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,33 @@
 
 class Contact
 {
-	private:
-		std::string firstName;
-		std::string lastName;
-		std::string nickname;
-		std::string phoneNumber;
-		std::string darkestSecret;
+private:
+	std::string	firstName;
+	std::string	lastName;
+	std::string	nickname;
+	std::string	phoneNumber;
+	std::string	darkestSecret;
 
-	public:
-		// ask user input to insert data fields
-		void set_data(std::string fname, std::string lname,
-			std::string nname, std::string pnumber, std::string dsecret)
-		{
-			firstName = fname;
-			lastName = lname;
-			nickname = nname;
-			phoneNumber = pnumber;
-			darkestSecret = dsecret;
-		}
-		// display all data fields
-		void display_data()
-		{
-			std::cout << "First Name: " << firstName << "\n";
-			std::cout << "Last Name: " << lastName << "\n";
-			std::cout << "Nickname: " << nickname << "\n";
-			std::cout << "Phone Number: " << phoneNumber << "\n";
-			std::cout << "Darkest Secret: " << darkestSecret << "\n";
-		}
+public:
+	// ask for user input to set data fields
+	void	set_data(std::string fname, std::string lname,
+		std::string nname, std::string pnumber, std::string dsecret)
+	{
+		firstName = fname;
+		lastName = lname;
+		nickname = nname;
+		phoneNumber = pnumber;
+		darkestSecret = dsecret;
+	}
+	// display all data fields
+	void	display_data()
+	{
+		std::cout << "First Name: " << firstName << "\n";
+		std::cout << "Last Name: " << lastName << "\n";
+		std::cout << "Nickname: " << nickname << "\n";
+		std::cout << "Phone Number: " << phoneNumber << "\n";
+		std::cout << "Darkest Secret: " << darkestSecret << "\n";
+	}
 };
 
 #endif
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: tmurua <tmurua@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 16:07:42 by tmurua            #+#    #+#             */
-/*   Updated: 2025/02/20 17:41:52 by tmurua           ###   ########.fr       */
+/*   Updated: 2025/02/20 20:08:36 by tmurua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,47 +17,54 @@
 
 class PhoneBook
 {
-	private:
-		Contact all_contacts[8];
-		int current_contact = 0; // where current contact will be stored
+private:
+	Contact	all_contacts[8];
+	int			current_contact; // where current contact will be stored
 
-	public:
-		void add_contact()
+public:
+	PhoneBook() // constructor method to initialize current_contact to 0
+	{
+		current_contact = 0;
+	}
+
+	void	add_contact()
+	{
+		Contact		contact;
+		std::string	input; // variable to temporarily store each user input
+		std::cout << "First Name: "; // ask input from user for each field
+		get_input(input);
+		std::string	fname = input;
+		std::cout << "Last Name: ";
+		get_input(input);
+		std::string	lname = input;
+		std::cout << "Nickname: ";
+		get_input(input);
+		std::string	nname = input;
+		std::cout << "Phone Number: ";
+		get_input(input);
+		std::string	pnumber = input;
+		std::cout << "Darkest Secret: ";
+		get_input(input);
+		std::string	dsecret = input;
+		// fill contact with provided data
+		contact.set_data(fname, lname, nname, pnumber, dsecret);
+		// save contact into array at current position
+		all_contacts[current_contact] = contact;
+		// update current_contact position (circular buffer: 0-7)
+		current_contact = (current_contact + 1) % 8;
+	}
+
+	void	get_input(std::string &input)
+	{
+		while (true) // keep prompting until a non-empty input is provided.
 		{
-			Contact contact;
-			std::string input; // variable to temporarily store each user input
-
-			std::cout << "First Name: "; // ask input from user for each field
 			// std::getline stores chars (up to \n) from std::cin into input
 			std::getline(std::cin, input);
-			// TODO: if input is empty, then ask again
-			std::string fname = input;
-
-			std::cout << "Last Name: ";
-			std::getline(std::cin, input);
-			std::string lname = input;
-
-			std::cout << "Nickname: ";
-			std::getline(std::cin, input);
-			std::string nname = input;
-
-			std::cout << "Phone Number: ";
-			std::getline(std::cin, input);
-			std::string pnumber = input;
-
-			std::cout << "Darkest Secret: ";
-			std::getline(std::cin, input);
-			std::string dsecret = input;
-
-			// fill contact with provided data
-			contact.set_data(fname, lname, nname, pnumber, dsecret);
-
-			// save contact into array at current position
-			all_contacts[current_contact] = contact;
-
-			// update current_contact position (circular buffer: 0-7)
-			current_contact = (current_contact + 1) % 8;
+			if (input != "")
+				break;
+			std::cout << "Error: Field cannot be empty. \nPlease enter a value: ";
 		}
+	}
 };
 
 #endif
