80fa0e2790077c304b2635d98468cbdd49fa8435
tmurua <tommurua@gmail.com>
2025-02-24 20:36:21 +0100

    feat: implement display_all_contacts() with fixed-width, truncation, and display_index() using atoi conversion

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: tmurua <tmurua@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 16:07:42 by tmurua            #+#    #+#             */
-/*   Updated: 2025/02/21 19:34:20 by tmurua           ###   ########.fr       */
+/*   Updated: 2025/02/24 20:24:30 by tmurua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include "Contact.hpp"
 #include <iomanip> // for std::setw
+#include <cstdlib> // for std::atoi
 
 class PhoneBook
 {
@@ -75,29 +76,48 @@ public:
 
 	void	display_all_contacts()
 	{
-		int	index;
+		int			index;
+		std::string	field;
 
 		std::cout << "     Index|First Name| Last Name|  Nickname| Phone Num\n";
 		index = 1;
 		while (index <= 8)
 		{
+		// std::setw sets width of output to 10 chars (right aligned by default)
 			std::cout << std::setw(10) << index << "|";
-			std::cout << std::setw(10) << all_contacts[index - 1].firstName << "|";
-			std::cout << std::setw(10) << all_contacts[index - 1].lastName << "|";
-			std::cout << std::setw(10) << all_contacts[index - 1].nickname << "|";
-			std::cout << std::setw(10) << all_contacts[index - 1].phoneNumber << "\n";
+			field = all_contacts[index -1].firstName;
+			std::cout << std::setw(10) << format_field(field) << "|";
+			field = all_contacts[index -1].lastName;
+			std::cout << std::setw(10) << format_field(field) << "|";
+			field = all_contacts[index -1].nickname;
+			std::cout << std::setw(10) << format_field(field) << "|";
+			field = all_contacts[index -1].phoneNumber;
+			std::cout << std::setw(10) << format_field(field) << "\n";
 			index++;
 		}
 	}
 
+	std::string	format_field(std::string field)
+	{
+	if (field.size()  > 10) // size() returns nbr of chars in field
+		return field.substr(0, 9) + "."; // substr(0, 9) extracts first 9 chars of field
+	return (field);
+	}
+
+
 	void	display_index()
 	{
-		int	index;
+		int			index;
+		std::string	input;
 
 		std::cout << "Enter a contact's Index to see more details on them:\n";
-		std::cin >> index;
+		std::getline(std::cin, input);
+	// c_str() converts input from C++ style to C-string, needed by atoi to parse it as int
+		index = std::atoi(input.c_str());
 		if (index >= 1 && index <= 8)
 			all_contacts[index - 1].display_data();
+		else
+			std::cout << "Invalid input. Please enter a number between 1 and 8.\n";
 	}
 };
 
